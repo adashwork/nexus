55c33174d36e3fb89e2705ae732c147be9093478
mosco0110 <mitsushima201810adash@outlook.jp>
2018-12-20 15:26:19 +0900

    Comment関連の編集

--- nexus/src/jp/or/adash/nexus/services/CommentService.java
+++ nexus/src/jp/or/adash/nexus/services/CommentService.java
@@ -1,5 +1,131 @@
 package jp.or.adash.nexus.services;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.or.adash.nexus.dao.CommentDao;
+import jp.or.adash.nexus.dao.SaibanDao;
+import jp.or.adash.nexus.entity.Comment;
+import jp.or.adash.nexus.utils.dao.Transaction;
+
 public class CommentService {
 
+	/**
+	 * トランザクションオブジェクト
+	 */
+	private Transaction transaction;
+
+	/**
+	 * 処理結果メッセージを格納するリスト
+	 */
+	private List<String> messages;
+
+	/**
+	 * メッセージのリストを取得する
+	 * @return  List<String>
+	 */
+	public List<String> getMessages() {
+		return messages;
+	}
+
+
+	public CommentService(){
+		transaction = new Transaction();
+		messages = new ArrayList<String>();
+	}
+
+
+	/**
+	 * 入力チェック
+	 * @param comment
+	 * @return checkResult true or false
+	 */
+	public boolean checkComment(Comment comment) {
+		boolean checkResult = true;
+
+		// フォーマットチェック
+		// 事業所NO（13桁）
+		if(comment.getCompanyNo() != null && !comment.getCompanyNo().equals("")) {
+			if(comment.getCompanyNo().length() != 13) {
+				messages.add("事業所NOは13桁で入力してください。");
+				checkResult = false;
+			}
+		}
+
+		// 求人NO（14桁）
+		if(comment.getKyujinNo() != null && !comment.getKyujinNo().equals("")) {
+			if(comment.getKyujinNo().length() != 14) {
+				messages.add("求人NOは14桁で入力してください。");
+				checkResult = false;
+			}
+		}
+
+		// カテゴリ選択「0：選択なし」ならエラー
+		if(comment.getGenre().equals("0")) {
+			messages.add("内容分類を選択してください。");
+			checkResult = false;
+		}
+
+		return checkResult;
+	}
+
+
+	/**
+	 * コメントの登録
+	 * @param Comment
+	 * @return true:成功時  false:失敗時
+	 */
+	public Comment insertComment(Comment comment) {
+		// TODO ここでIDを入れなおす
+		boolean result = false; // 処理結果
+
+		try {
+			// データベース接続を開始する
+			transaction.open();
+
+			// トランザクションを開始する
+			transaction.beginTrans();
+
+			//采番マスタよりデータ取得
+			SaibanDao saidao = new SaibanDao(transaction);
+			int saiban = saidao.getsaiban();
+
+			//とってきた番号を加工し、Kyujin.noにデータ格納
+			String str = String.format("A" + "%013d", saiban);
+			// comment.setId(str);
+
+			CommentDao commentDao = new CommentDao(transaction);
+			int count = commentDao.insert(comment);
+
+			if (count > 0) {
+				// 完了メッセージをセットする
+				messages.add("登録が完了しました");
+
+
+			} else {
+				// エラーメッセージをセットする
+				messages.add("登録に失敗しました");
+				comment.setId(null);
+
+
+			}
+
+			// トランザクションをコミットする
+			transaction.commit();
+
+		} catch (IOException e) {
+			// トランザクションをロールバックする
+			transaction.rollback();
+
+			// エラーメッセージをセットする
+			messages.add("データベースアクセスに失敗しました。");
+		} finally {
+			// データベース接続をを終了する
+			transaction.close();
+		}
+
+		return comment;
+
+	}
 }
--- nexus/src/jp/or/adash/nexus/servlets/CommentRegistServlet.java
+++ nexus/src/jp/or/adash/nexus/servlets/CommentRegistServlet.java
@@ -1,11 +1,16 @@
 package jp.or.adash.nexus.servlets;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jp.or.adash.nexus.entity.Comment;
+import jp.or.adash.nexus.services.CommentService;
 
 /**
  * Servlet implementation class CommentRegistServlet
@@ -13,29 +18,68 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CommentRegistServlet")
 public class CommentRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public CommentRegistServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		// 入力されたパラメータ等の取得
+		Integer id = 0;											// 備考ID
+		String companyNo = request.getParameter("");			// 事業所番号
+		String kyujinNo = request.getParameter("");			// 求人NO
+		String jobSeekerId = request.getParameter("");			// 求職者ID
+		String staffId = request.getParameter("");				// 職業紹介者ID
+		Integer matchId = -1;								// マッチング事例ID
+		String genre = request.getParameter("");				// 内容分類
+		String important = request.getParameter("");			// 重要アラート
+		String title = request.getParameter("");				// 件名
+		String note = request.getParameter("");					// 備考
+		Date createDt = null;									// 新規登録日
+		String createUserId = request.getParameter("");		// 新規登録ユーザー
+		Date updateDt = null;									// 最終更新日
+		String updateUserId = request.getParameter("");		// 最終更新ユーザー
+
+		if(request.getParameter("マッチングID") != null && !request.getParameter("マッチングID").equals("")) {
+			matchId = Integer.parseInt(request.getParameter("マッチングID"));
+		}
+
+		// コメントオブジェクト作成
+		Comment comment = new Comment(id,
+										companyNo,
+										kyujinNo,
+										jobSeekerId,
+										staffId,
+										matchId,
+										genre,
+										important,
+										title,
+										note,
+										createDt,
+										createUserId,
+										updateDt,
+										updateUserId);
+
+		CommentService commentService = new CommentService();
+		// 入力チェック
+		if(commentService.checkComment(comment)) {
+			comment = commentService.insertComment(comment);
+		}
+		// trueならIDも返す、falseならIDは返さない
+		request.setAttribute("comment", comment);
+		request.setAttribute("messages", commentService.getMessages());
+
+		// JSPにフォワード
+		request.getRequestDispatcher("/companyregist.jsp")
+				.forward(request, response);
+
 	}
 
 }

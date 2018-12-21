43a26c3336cc6424b5b54e3a1910715ad5786e4d
kitayama <kitayama201810adash@outlook.jp>
2018-12-21 14:13:32 +0900

    暫定完成

--- nexus/src/jp/or/adash/nexus/servlets/CommentUpdateServlet.java
+++ nexus/src/jp/or/adash/nexus/servlets/CommentUpdateServlet.java
@@ -1,6 +1,7 @@
 package jp.or.adash.nexus.servlets;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -10,12 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import jp.or.adash.nexus.entity.Comment;
 import jp.or.adash.nexus.entity.Staff;
+import jp.or.adash.nexus.services.CommentService;
 
 /**
- * Servlet implementation class CommentUpdateServlet
+ * コメントテーブルを更新するサーブレット
+ * @author kitayama
  */
-@WebServlet("/comment-update")
+@WebServlet("/web/comment-update")
 public class CommentUpdateServlet extends HttpServlet {
 	 static final long serialVersionUID = 1L;
 
@@ -35,19 +39,73 @@ public class CommentUpdateServlet extends HttpServlet {
 		HttpSession session = request.getSession(true);
 		Staff staff = (Staff) session.getAttribute("UserData");
 
-		 Integer id = -1;
-		 String companyNo = request.getParameter("companyno");
-		 String kyujinNo = request.getParameter("");
-		 String jobSeekerId = request.getParameter("");
-		 String staffId = request.getParameter("");
-		 Integer matchId = request.getParameter("");
-		 String genre = request.getParameter("");
-		 String important = request.getParameter("");
-		 String title = request.getParameter("");
-		 String note = request.getParameter("");
-		 Date updateDt;
-		 String updateUserId;
 
+		// 各パラメータの取得
+		Integer id = Integer.parseInt(request.getParameter("commentid"));	// コメントID
+		String companyNo = request.getParameter("companyno");				// 事業所番号
+		String kyujinNo = request.getParameter("kyujinno");				// 求人番号
+		String jobSeekerId = request.getParameter("jobseekeeid");			// 求職者ID
+		String staffId = request.getParameter("staffid");					// 職業紹介者ID
+		Integer matchId = -1;												// マッチングID
+		String genre = request.getParameter("genre");						// 内容分類
+		String important = request.getParameter("important");				// 重要度
+		String title = request.getParameter("title");						// 件名
+		String note = request.getParameter("note");						// 備考
+		Date createDt = null;												// 新規登録日
+		String createUserId = request.getParameter("createuserid");		// 新規登録ユーザー
+		Date updateDt = null;												// 最終更新日
+		String updateUserId = staff.getId();								// 最終更新ユーザー
+
+		// matchidをint型に変換
+		if(request.getParameter("matchid") != null && !request.getParameter("matchid").equals("")) {
+			 matchId = Integer.parseInt(request.getParameter("matchid"));
+		}
+
+		// createdt, updatedtをDate型に変換
+		try {
+			createDt = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("createdt"));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			updateDt = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("updatedt"));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		// オブジェクトに格納
+		Comment comment = new Comment(
+				 						id
+				 						,companyNo
+				 						,kyujinNo
+				 						,jobSeekerId
+				 						,staffId
+				 						,matchId
+				 						,genre
+				 						,important
+				 						,title
+				 						,note
+				 						,createDt
+				 						,createUserId
+				 						,updateDt
+				 						,updateUserId
+				 						);
+
+		// コメントをDBに登録
+		CommentService service = new CommentService();
+		if(service.checkComment(comment)) {
+			service.commentUpdate(comment);
+		}
+
+		// requsetにパラメータを格納
+		request.setAttribute("Staff", staff);
+		request.setAttribute("comment", comment);
+		request.setAttribute("messages", service.getMessages());
+
+		// JSPにフォワード
+		request.getRequestDispatcher("/commentregist.jsp")
+				.forward(request, response);
 
 
 

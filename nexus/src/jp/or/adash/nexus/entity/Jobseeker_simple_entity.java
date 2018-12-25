3c3d1b3f798c5f2099346e7b6fcac0496557a5f8
AWU309 <AWU309@AW309.adash.local>
2018-12-25 12:54:01 +0900

    jobseeker_simple_entityを3ヶ月前の状態に戻しました。おそらく、検索ではこの状態のものにあわせて開発していたはずです。

--- nexus/src/jp/or/adash/nexus/entity/Jobseeker_simple_entity.java
+++ nexus/src/jp/or/adash/nexus/entity/Jobseeker_simple_entity.java
@@ -3,31 +3,22 @@ package jp.or.adash.nexus.entity;
 public class Jobseeker_simple_entity {
 	private String id;
 	private String js_name;
-//	private Integer age;
+	private Integer age;
 	private String sex;
-	private String HOPEJOB1;
-	private String HOPEJOB2;
-	private String HOPEJOB3;
-	private String HOPEJOBCATEGORY;
-	private String HOPEJOBCATEGORY2;
-	private String HOPEJOBCATEGORY3;
-
+	private String hopejobcategory;
+	private String hopejob1;
 	private String hopeworkplace;
 	private String st_name;
 
-	public Jobseeker_simple_entity(String id, String js_name,
-			String sex, String HOPEJOB1, String HOPEJOB2, String HOPEJOB3, String HOPEJOBCATEGORY, String HOPEJOBCATEGORY2,String HOPEJOBCATEGORY3,
-			String hopeworkplace,String st_name) {
+	public Jobseeker_simple_entity(String id, String js_name,Integer age,
+			String sex,String hopejobcategory,
+			String hopejob1,String hopeworkplace,String st_name) {
 		this.id = id;
 		this.js_name = js_name;
-//		this.age = age;
+		this.age = age;
 		this.sex = sex;
-		this.HOPEJOB1 = HOPEJOB1;
-		this.HOPEJOB2 = HOPEJOB2;
-		this.HOPEJOB3 = HOPEJOB3;
-		this.HOPEJOBCATEGORY = HOPEJOBCATEGORY ;
-		this.HOPEJOBCATEGORY2 = HOPEJOBCATEGORY2 ;
-		this.HOPEJOBCATEGORY3 = HOPEJOBCATEGORY3 ;
+		this.hopejobcategory = hopejobcategory ;
+		this.hopejob1 = hopejob1;
 		this.hopeworkplace = hopeworkplace;
 		this.st_name = st_name;
 	}
@@ -40,36 +31,20 @@ public class Jobseeker_simple_entity {
 		return js_name;
 	}
 
-/*	public Integer getAge() {
+	public Integer getAge() {
 		return age;
 	}
-*/
+
 	public String getSex() {
 		return sex;
 	}
 
-
-	public String getHOPEJOB1() {
-		return HOPEJOB1;
+	public String getHopejobcategory() {
+		return hopejobcategory;
 	}
 
-	public String getHOPEJOB2() {
-		return HOPEJOB2;
-	}
-	public String getHOPEJOB3() {
-		return HOPEJOB3;
-	}
-
-	public String getHOPEJOBCATEGORY() {
-		return HOPEJOBCATEGORY;
-	}
-
-	public String getHOPEJOBCATEGORY2() {
-		return HOPEJOBCATEGORY2;
-	}
-
-	public String getHOPEJOBCATEGORY3() {
-		return HOPEJOBCATEGORY3;
+	public String getHopejob1() {
+		return hopejob1;
 	}
 
 	public String getHopeworkplace() {

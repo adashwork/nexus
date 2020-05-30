b1a88ecb2b39f82948e8f0e35a7aa869b8e4a38e
terukawasaki <adash-kawasaki@outlook.jp>
2020-05-30 11:11:53 +0900

    doPostを元に戻す

--- nexus/src/jp/or/adash/nexus/servlets/JobSeekerSearchServlet.java
+++ nexus/src/jp/or/adash/nexus/servlets/JobSeekerSearchServlet.java
@@ -32,10 +32,10 @@ public class JobSeekerSearchServlet extends HttpServlet {
 		super();
 	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		doPost(request, response);
-//	}
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
+		doPost(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		HttpSession session = request.getSession(true);
 		Staff staff = (Staff) session.getAttribute("UserData");
